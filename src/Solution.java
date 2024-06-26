1d41a86d617ad62a68e7c7586218c895d5e4c52f
İsmet İnan <ismet11inan@gmail.com>
2024-06-26 11:09:55 +0300

    first commit

---
+++ src/Solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode node = new ListNode(0);
+        ListNode current = node;
+        
+        while (list1 != null && list2 != null) {
+            if ( list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        if ( list1 != null) {
+            current.next = list1;
+            list1 = list1.next;
+        }
+        if ( list2 != null) {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        return node.next;
+    }
+}
